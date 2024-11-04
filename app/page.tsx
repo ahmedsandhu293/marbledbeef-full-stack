4c700fa7fbc8610cf481de98925a5ed983912a8a
arslanzaki <arslanzaki223@gmail.com>
2024-11-05 03:04:44 +0500

    setup nextui

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+import { Button } from "@nextui-org/button";
+
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <h1>MarbledBeef</h1>
+
+      <Button>Shop Now</Button>
+    </section>
+  );
+}
