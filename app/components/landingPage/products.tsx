3653fc525a5188d21c39d23cb84f87fbd228992b
hafiz-asadpbg <asadullahofficial009@gmail.com>
2024-11-24 19:30:15 +0500

    add headings

--- app/components/landingPage/products.tsx
+++ app/components/landingPage/products.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 // Custome Component
 import Cardsompoent from "../common/cards/Cards";
 
@@ -15,90 +14,82 @@ import HorizentalCarousel from "../common/carousel";
 import CategoriesHeading from "../common/headings/categoriesHeading";
 
 const Products = () => {
+  const handleAddToCart = (item: ProductsType) => {
+    console.log("Added to cart:", item);
+  };
 
+  const handleAddToFavorite = (item: ProductsType) => {
+    console.log("Added to favorite:", item);
+  };
 
-    const handleAddToCart = (item: ProductsType) => {
-        console.log("Added to cart:", item);
-    };
+  const handleClick = (item: ProductsType) => {
+    console.log("Card clicked:", item);
+  };
 
-    const handleAddToFavorite = (item: ProductsType) => {
-        console.log("Added to favorite:", item);
-    };
-
-    const handleClick = (item: ProductsType) => {
-        console.log("Card clicked:", item);
-    };
-
-    return (
-        <div className="w-full space-y-4 md:space-y-8">
-            <div>
-
-                <CategoriesHeading title="Sélection d'excellence" onClick={() => { console.log("Explore all") }} />
-                <HorizentalCarousel autoPlaySpeed={4000} infinite={true}>
-                    {
-                        dummyData.map((item, index) => (
-                            <div
-                                key={index}
-                                style={{ padding: "10px", textAlign: "center" }}
-                            >
-                                <CardCompoent
-                                    data={item}
-                                    onAddToCart={handleAddToCart}
-                                    onAddToFavorite={handleAddToFavorite}
-                                    onClick={handleClick}
-                                />
-                            </div>
-                        ))
-                    }
-
-                </HorizentalCarousel>
+  return (
+    <div className="w-full space-y-4 md:space-y-8">
+      <div>
+        <CategoriesHeading
+          title="Sélection d'excellence"
+          onClick={() => {
+            console.log("Explore all");
+          }}
+        />
+        <HorizentalCarousel autoPlaySpeed={4000} infinite={true}>
+          {dummyData.map((item, index) => (
+            <div key={index} style={{ padding: "10px", textAlign: "center" }}>
+              <CardCompoent
+                data={item}
+                onAddToCart={handleAddToCart}
+                onAddToFavorite={handleAddToFavorite}
+                onClick={handleClick}
+              />
             </div>
-            <div>
-
-                <CategoriesHeading title="Sélection d'excellence" onClick={() => { console.log("Explore all") }} />
-                <HorizentalCarousel autoPlaySpeed={4000} infinite={true}>
-                    {
-                        dummyData.map((item, index) => (
-                            <div
-                                key={index}
-                                style={{ padding: "10px", textAlign: "center" }}
-                            >
-                                <CardCompoent
-                                    data={item}
-                                    onAddToCart={handleAddToCart}
-                                    onAddToFavorite={handleAddToFavorite}
-                                    onClick={handleClick}
-                                />
-                            </div>
-                        ))
-                    }
-
-                </HorizentalCarousel>
+          ))}
+        </HorizentalCarousel>
+      </div>
+      <div>
+        <CategoriesHeading
+          title="Sélection d'excellence"
+          onClick={() => {
+            console.log("Explore all");
+          }}
+        />
+        <HorizentalCarousel autoPlaySpeed={4000} infinite={true}>
+          {dummyData.map((item, index) => (
+            <div key={index} style={{ padding: "10px", textAlign: "center" }}>
+              <CardCompoent
+                data={item}
+                onAddToCart={handleAddToCart}
+                onAddToFavorite={handleAddToFavorite}
+                onClick={handleClick}
+              />
             </div>
-            <div>
-
-                <CategoriesHeading title="Sélection d'excellence" onClick={() => { console.log("Explore all") }} />
-                <HorizentalCarousel autoPlaySpeed={4000} infinite={true}>
-                    {
-                        dummyData.map((item, index) => (
-                            <div
-                                key={index}
-                                style={{ padding: "10px", textAlign: "center" }}
-                            >
-                                <CardCompoent
-                                    data={item}
-                                    onAddToCart={handleAddToCart}
-                                    onAddToFavorite={handleAddToFavorite}
-                                    onClick={handleClick}
-                                />
-                            </div>
-                        ))
-                    }
-
-                </HorizentalCarousel>
+          ))}
+        </HorizentalCarousel>
+      </div>
+      <div>
+        <CategoriesHeading
+          title="Sélection d'excellence"
+          onClick={() => {
+            console.log("Explore all");
+          }}
+        />
+        <HorizentalCarousel autoPlaySpeed={4000} infinite={true}>
+          {dummyData.map((item, index) => (
+            <div key={index} style={{ padding: "10px", textAlign: "center" }}>
+              <CardCompoent
+                data={item}
+                onAddToCart={handleAddToCart}
+                onAddToFavorite={handleAddToFavorite}
+                onClick={handleClick}
+              />
             </div>
-        </div>
-    );
+          ))}
+        </HorizentalCarousel>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
