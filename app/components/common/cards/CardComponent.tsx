0f8977542684eded2ad583ed43dbaf95799c1b4b
hafiz-asadpbg <asadullahofficial009@gmail.com>
2024-12-21 03:42:55 +0500

    feat: design landing page

--- app/components/common/cards/CardComponent.tsx
+++ app/components/common/cards/CardComponent.tsx
@@ -23,10 +23,10 @@ const CardComponent: React.FC<CardProps> = ({
           onClick(data);
         }}
       >
-        <CardBody className="overflow-visible p-0">
+        <CardBody className="overflow-visible p-0 ">
           <Image
             alt={data.title}
-            className="w-full object-cover h-[200px]"
+            className="w-full object-cover h-[200px] border-2 border-gold"
             radius="lg"
             shadow="sm"
             src={data.img}
@@ -35,18 +35,18 @@ const CardComponent: React.FC<CardProps> = ({
         </CardBody>
         <CardFooter className="flex flex-col gap-2">
           <div className="flex justify-between w-full">
-            <b className="truncate text-white">{data.title}</b>
-            <p className="text-default-500">{data.price}</p>
+            <b className="truncate text-text-gold">{data.title}</b>
+            <p className="text-default-500 text-text-gold">{data.price}</p>
           </div>
 
           <div className="flex justify-between items-center w-full gap-4">
             <ComponentButton
-              className="!w-full !bg-red-primary"
+              className="!w-full !bg-red-primary !bg-gradient-primary"
               label="Add to cart"
               onClick={() => onAddToCart(data)}
             />
             <FiHeart
-              className="text-gray-500 hover:text-red-primary cursor-pointer"
+              className="text-text-gold hover:text-red-primary cursor-pointer"
               size={28}
               onClick={(e) => {
                 e.stopPropagation();
--- app/components/nav/footer.tsx
+++ app/components/nav/footer.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@nextui-org/button";
 import Link from "next/link";
 import { FaTwitter, FaYoutube } from "react-icons/fa";
 
@@ -5,80 +6,70 @@ export default function Footer() {
   return (
     <footer className="text-text-primary py-10 px-6 border-t border-border-primary bg-background-primary">
       <div className="container mx-auto">
-        <div className="flex flex-col space-y-8 md:flex-row md:space-y-0 md:space-x-8 justify-between items-center md:items-start">
+        <div className="grid grid-cols-10 space-y-8 md:flex-row md:space-y-0 md:space-x-8 justify-between items-center md:items-start">
           {/* Left section */}
-          <div className="text-left space-y-4 max-w-xs md:max-w-none w-full">
+          <div className="col-span-4 text-left space-y-4 max-w-xs md:max-w-none w-full">
             <h1 className="text-2xl font-bold">LOGO</h1>
-            <p className="text-text-secondary">Get 15% off your first order!</p>
-            <p className="text-text-secondary">
-              Sign up to our mailing list below to get 15% off your first order.
-              Don&apos;t worry, we hate spam too.
-            </p>
-            <div className="flex mt-4 w-full">
-              <input
-                className="w-full md:w-auto flex bg-background-secondary border border-border-primary rounded-l-lg p-2 text-text-primary focus:outline-none"
-                placeholder="Your email address"
-                type="email"
-              />
-              <button className="bg-text-primary text-background-primary rounded-r-lg px-4 py-2">
-                Subscribe
-              </button>
-            </div>
+            <p className="text-text-secondary">Boucherie en ligne agréé pour le bœuf de Kobé et le Wagyu. Découvrez aussi notre sélection de viandes maturées, le bœuf de Galice, l'Angus, Salers, la Normande, ainsi que le porc Ibérique Bellota Pata Negra. Toutes nos viandes sont rigoureusement sélectionnées pour une expérience gustative exceptionnelle. Marbled Beef est également le fournisseur de choix pour tous les professionnels de la gastronomie à la recherche dune expertise de qualité.</p>
+
+
+            <Button className="bg-gold text-background-primary rounded-lg px-4 py-2">
+              Contactez-nous
+            </Button>
           </div>
 
           {/* Center section */}
-          <div className="grid grid-cols-1 gap-x-4 gap-y-6 text-left md:flex md:justify-start md:space-x-8 text-sm w-full md:w-auto">
-            <div>
-              <h3 className="font-semibold">Products</h3>
-              <ul className="space-y-2">
-                <li>
-                  <Link href="/templates">Templates</Link>
-                </li>
-                <li>
-                  <Link href="/backgrounds">Backgrounds</Link>
-                </li>
-                <li>
-                  <Link href="/mockups">Mockups</Link>
-                </li>
-                <li>
-                  <Link href="/fonts">Fonts</Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="font-semibold">All-Access Pass</h3>
-              <ul className="space-y-2">
-                <li>
-                  <Link href="/signup">Sign Up</Link>
-                </li>
-                <li>
-                  <Link href="/activate-license">Activate License</Link>
-                </li>
-                <li>
-                  <Link href="/signin">Sign In</Link>
-                </li>
-                <li>
-                  <Link href="/reset-password">Reset Password</Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="font-semibold">Information</h3>
-              <ul className="space-y-2">
-                <li>
-                  <Link href="/faq">FAQ</Link>
-                </li>
-                <li>
-                  <Link href="/contact-us">Contact Us</Link>
-                </li>
-                <li>
-                  <Link href="/license-agreement">License Agreement</Link>
-                </li>
-                <li>
-                  <Link href="/privacy-policy">Privacy Policy</Link>
-                </li>
-              </ul>
-            </div>
+          <div className="col-span-2">
+            <h3 className="font-bold pb-2">MARBLED BEEF VIANDES
+              D’EXCELLENCE</h3>
+            <ul className="space-y-2">
+              <li>
+                <Link href="/templates">Templates</Link>
+              </li>
+              <li>
+                <Link href="/backgrounds">Backgrounds</Link>
+              </li>
+              <li>
+                <Link href="/mockups">Mockups</Link>
+              </li>
+              <li>
+                <Link href="/fonts">Fonts</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-span-2">
+            <h3 className="font-bold pb-2">NOS SERVICES</h3>
+            <ul className="space-y-2">
+              <li>
+                <Link href="/signup">Sign Up</Link>
+              </li>
+              <li>
+                <Link href="/activate-license">Activate License</Link>
+              </li>
+              <li>
+                <Link href="/signin">Sign In</Link>
+              </li>
+              <li>
+                <Link href="/reset-password">Reset Password</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-span-2">
+            <h3 className="font-bold pb-2">INFOS LÉGALES</h3>
+            <ul className="space-y-2">
+              <li>
+                <Link href="/faq">FAQ</Link>
+              </li>
+              <li>
+                <Link href="/contact-us">Contact Us</Link>
+              </li>
+              <li>
+                <Link href="/license-agreement">License Agreement</Link>
+              </li>
+              <li>
+                <Link href="/privacy-policy">Privacy Policy</Link>
+              </li>
+            </ul>
           </div>
         </div>
 
