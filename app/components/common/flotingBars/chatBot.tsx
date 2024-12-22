b147a72a1e3683debb70361e15f93962362cde9b
hafiz-asadpbg <asadullahofficial009@gmail.com>
2024-12-22 11:54:29 +0500

    feat: chat bot fixes

--- app/components/common/flotingBars/chatBot.tsx
+++ app/components/common/flotingBars/chatBot.tsx
@@ -37,10 +37,7 @@ console.log("🚀 ~ prebuiltQuestions:", prebuiltQuestions);
 
 const Chatbot: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [messages, setMessages] = useState<Message[]>([
-    { text: "Hello! How can I assist you?", isBot: true },
-    { text: "Do you have Beef in stock? ", isBot: false },
-  ]);
+  const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [showPrebuiltQuestions, setShowPrebuiltQuestions] =
@@ -80,7 +77,7 @@ const Chatbot: React.FC = () => {
   }, [messages]);
 
   return (
-    <div className="fixed right-0 bottom-0 pb-2 sm:pb-5 z-10">
+    <div className="fixed right-0 bottom-0 pb-2 sm:pb-5 z-50 font-urbanist">
       <div className="relative mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <button
           className="bg-[#CF2D2D] text-white dark:text-zinc-50 p-3 rounded-md shadow-lg transition-all duration-500 ease-in-out"
@@ -108,11 +105,15 @@ const Chatbot: React.FC = () => {
               Marbled Beef <span className="font-bold"> AI Chatbot</span>
             </div>
 
+            <h2 className="text-3xl font-bold px-4 font-urbanist text-white">
+              Hello, I’m your AI Assistant!
+            </h2>
+
             <div className="p-4 chat-content h-80 overflow-y-auto">
               {messages.map((msg, index) => (
                 <div key={index} className="space-y-3">
                   {msg.isBot ? (
-                    <div className="bg-button-primary border border-button-primary text-zinc-900 dark:text-zinc-50 p-2 w-2/3 rounded-tl-md rounded-tr-md rounded-br-none rounded-bl-md text-sm mb-3">
+                    <div className="bg-button-primary border border-gold text-zinc-900 dark:text-zinc-50 p-2 w-2/3 rounded-tl-md rounded-tr-md rounded-br-none rounded-bl-md text-sm mb-3">
                       {msg.text}
                     </div>
                   ) : (
@@ -131,31 +132,31 @@ const Chatbot: React.FC = () => {
                 </div>
               )}
 
-              {/* {showPrebuiltQuestions && (
-                <div className="mt-4 border-t border-gray-300 dark:border-gray-700">
-                  <h3 className="text-sm font-semibold text-zinc-900 dark:text-gray-300 mb-2 pt-1">
-                    Questions rapides:
+              {showPrebuiltQuestions && (
+                <div className="mt-4 border border-gold p-4 rounded-2xl">
+                  <h3 className="text-sm font-semibold text-white mb-2 py-1">
+                    Questions rapides::
                   </h3>
                   <div className="space-y-2">
                     {prebuiltQuestions.map((question, index) => (
                       <button
                         key={index}
                         onClick={() => handleSendMessage(question)}
-                        className="block w-full text-left p-2 bg-gray-300 dark:bg-gray-700 text-zinc-900 dark:text-white rounded-md hover:bg-gray-200 dark:hover:bg-gray-600"
+                        className="bg-zinc-200 text-black  p-2 w-full rounded-tl-none rounded-tr-md rounded-br-md rounded-bl-md text-sm self-end mb-1"
                       >
                         {question}
                       </button>
                     ))}
                   </div>
                 </div>
-              )} */}
+              )}
 
               <div ref={chatEndRef} />
             </div>
 
-            <div className="py-2 px-4 border-t border-border-primary bg-button-primary flex gap-2">
+            <div className="py-2 px-4 border-t border-gold bg-button-primary flex gap-2">
               <input
-                className="w-full p-2 bg-transparent rounded-xl border border-gray-100  text-zinc-50"
+                className="w-full p-2 bg-transparent rounded-xl border border-gold  text-zinc-50"
                 disabled={loading}
                 placeholder="Type a message..."
                 type="text"
