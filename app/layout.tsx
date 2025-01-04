c87b7b0694365aa7e2571d681b3eae53e07d1736
hafiz-asadpbg <asadullahofficial009@gmail.com>
2025-01-04 13:30:59 +0500

    feat:loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata, Viewport } from "next";
 import clsx from "clsx";
 import dynamic from "next/dynamic";
 import { Urbanist } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -63,6 +64,23 @@ export default function RootLayout({
       >
         <GlobalContextProvider>
           <Navbar />
+          <NextTopLoader
+            color="#B28A3B"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={false}
+            showSpinner={true}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+            template='<div class="bar" role="bar"><div class="peg"></div></div> 
+            
+            <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+            zIndex={1600}
+            showAtBottom={false}
+          />
+
           <main className="pt-48  flex-grow">{children}</main>
           <Footer />
 
